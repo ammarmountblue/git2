bfb93deee7969b1196c29e79cc7905adc05609d5
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-07 17:35:51 +0530

    practice

---
+++ git.java
@@ -0,0 +1,8 @@
+class Git {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        for(int i = 0;i<10;i++){
+            System.out.println("Count: " + i);
+        }
+    }
+}
