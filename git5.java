c527189160593f7edcb35bfe3616a756b74367dd
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-07 18:01:31 +0530

    changes

---
+++ git5.java
@@ -0,0 +1,8 @@
+public class git5 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        for(int i = 0;i<10;i++){
+            System.out.println("Count: " + i);
+        }
+    }
+}
---
+++ git8.java
@@ -0,0 +1,14 @@
+public class git8 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        for(int i = 0;i<10;i++){
+            System.out.println("Count: " + i);
+            for(int j=0;j<5;j++){
+                System.out.println("Inner Count: " + j);
+            }
+                for(int k=0;k<3;k++){
+                    System.out.println("Innermost Count: " + k);
+                }
+        }
+    }  
+}
