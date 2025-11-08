8208fa7aab132145fd820a9c55e3227e09c6223c
Ammar Hasan <ammar.hasan.35.3@mountblue.tech>
2025-11-08 15:26:47 +0530

    km

--- git8.java
+++ git8.java
@@ -1,5 +1,6 @@
 public class git8 {
     public static void main(String[] args) {
+        System.out.println("Git version control example.");
         System.out.println("Hello, Git!");
         for(int i = 0;i<10;i++){
             System.out.println("Count: " + i);
